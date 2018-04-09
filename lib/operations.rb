2d17059f2311db987a6b0c3687a551c6a988018c
Dani Lyn <dlyn1110@gmail.com>
2018-04-09 13:30:28 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,12 @@
 def unsafe?(speed)
-if speed > 60 && < 40
+if speed > 60 &&  speed < 40
 	true
-else 
+else
 	return false
 end
 
 
 
 def not_safe?(speed)
-	speed > 40 && > 60 ? true : false
+	speed > 40 && speed > 60 ? true : false
 end
-	
-
-
